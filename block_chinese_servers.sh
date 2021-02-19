5e8a5ef048f2004543f2e6ff27f91da95f86ae9b
KaMyKaSii <KaMyKaSii@users.noreply.github.com>
2021-02-19 20:14:49 -0300

    Update and rename unlock_ssh_and_disable_chinese_servers.sh to block_chinese_servers.sh

--- block_chinese_servers.sh
+++ block_chinese_servers.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-nvram set ssh_en=1
-nvram set uart_en=1
-nvram set boot_wait=on
-nvram commit
-cp /etc/init.d/dropbear /etc/init.d/dropbear_backup
-sed -i '/flg_ssh.*release/ { :a; N; /fi/! ba };/return 0/d' /etc/init.d/dropbear
-echo -e "password\npassword" | passwd root
-/etc/init.d/dropbear enable
-/etc/init.d/dropbear start
-#Try to preserve modified dropbear file on upgrade
-echo "/etc/init.d/dropbear" >> /etc/sysupgrade.conf
-
 cat << 'EOF' >> /etc/hosts
 
 ::1     localhost ip6-localhost ip6-loopback
@@ -27,5 +15,3 @@ ff02::2 ip6-allrouters
 0.0.0.0 bbs.xiaomi.cn bbs.xiaomi.com
 ::1     bbs.xiaomi.cn bbs.xiaomi.com
 EOF
-
-reboot
