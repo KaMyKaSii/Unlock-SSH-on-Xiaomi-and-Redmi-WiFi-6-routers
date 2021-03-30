51cdf84eb1a6a053fbe0c3bf210fc9292ba91e3a
KaMyKaSii <KaMyKaSii@users.noreply.github.com>
2021-03-30 17:36:00 -0300

    Update unlock_ssh.sh

--- unlock_ssh.sh
+++ unlock_ssh.sh
@@ -6,7 +6,7 @@ nvram set boot_wait=on
 nvram commit
 cp /etc/init.d/dropbear /etc/init.d/dropbear_backup
 sed -i '/flg_ssh.*release/ { :a; N; /fi/! ba };/return 0/d' /etc/init.d/dropbear
-echo -e "password\npassword" | passwd root
+echo -e 'password\npassword' | passwd root
 /etc/init.d/dropbear enable
 /etc/init.d/dropbear start
 #Try to preserve modified dropbear file on upgrade
